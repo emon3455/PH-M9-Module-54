c238f9c949d2c6358325bd5e3ea701848ed8259a
Emon <emon.mhk69@gmail.com>
2023-04-03 23:39:45 +0600

    friend done

--- src/Components/Friend/Friend.jsx
+++ src/Components/Friend/Friend.jsx
@@ -10,7 +10,7 @@ const Friend = ({friend}) => {
             <h3 className='text-2xl font-bold'>{name}</h3>            
             <h4 className='text-lg'>Mail: {email}</h4>
             <h4 className='text-lg'>Phone: {phone}</h4>
-            <p className='px-2 py-1 bg-sky-500 text-white font-semibold rounded-lg'>
+            <p className='bg-sky-500 text-white font-semibold rounded-lg'>
                 <Link to={`/friend/${id}`}>Know More</Link>
             </p>
         </div>
--- src/Components/SingleFriend/SingleFriend.jsx
+++ src/Components/SingleFriend/SingleFriend.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const SingleFriend = () => {
     const friend = useLoaderData();
     const {name,address,company,phone} = friend;
+
+    const navigate = useNavigate();
+    const handleNavigate =()=>{
+        navigate(-1);
+    }
+
     return (
         <div>
             <div className='shadow-2xl p-4 text-center border mt-4'>
@@ -11,6 +17,7 @@ const SingleFriend = () => {
                 <h4 className='text-lg'>Adderess: {address.city}</h4>
                 <h4 className='text-lg'>Works IN: {company.name}</h4>
                 <h4 className='text-lg'>Phone: {phone}</h4>
+                <button className='bg-sky-300 p-1 rounded-lg' onClick={handleNavigate}>Go Back</button>
             </div>
         </div>
     );
